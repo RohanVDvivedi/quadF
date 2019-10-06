f53c3dafe53ee2975dc14156da774a98640bf22d
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-10-06 18:31:07 +0530

    i2c simple sensoe read implementation done

--- main/main.c
+++ main/main.c
@@ -9,16 +9,37 @@
 
 #define BLINK_GPIO 2
 
+#define I2C_MASTER_SCL_IO   22
+#define I2C_MASTER_SDA_IO   21
+
+// i2c addresses, for connected sensors namesly MPU6050 : accl/gyro, HMC5883L : magnetometer and MS5611 : barometer
+#define MPU6050_ADDRESS     0x68
+#define HMC5883L_ADDRESS    0x1e
+#define MS5611_ADDRESS      0x76
+
+// motor pins
+#define LEFT_FRONT_MOTOR    33
+#define RIGHT_FRONT_MOTOR   32
+#define LEFT_BACK_MOTOR     25
+#define RIGHT_BACK_MOTOR    26
+
+void i2c_init();
+void i2c_read(uint8_t device_address, unsigned int address, void* buffer, unsigned int bytes_to_read);
+void i2c_write(uint8_t device_address, unsigned int address, void* buffer, unsigned int bytes_to_write);
 void write_values_bldc(unsigned int left_front, unsigned int right_front, unsigned int left_back, unsigned int right_back);
 
 void app_main(void)
 {
+    /*
     // motor startup and max min configuring 
     // this is the first call hence the motors will be initalized also to their max values
     write_values_bldc(1000, 1000, 1000, 1000);
     vTaskDelay(4000 / portTICK_PERIOD_MS);
     write_values_bldc(0, 0, 0, 0);
     vTaskDelay(3000 / portTICK_PERIOD_MS);
+    */
+
+    i2c_init();
 
     gpio_pad_select_gpio(BLINK_GPIO);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
@@ -27,33 +48,86 @@ void app_main(void)
         gpio_set_level(BLINK_GPIO, 1);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
         gpio_set_level(BLINK_GPIO, 0);
-
-        write_values_bldc(1000, 0, 0, 0);
-
-        vTaskDelay(10000 / portTICK_PERIOD_MS);
-
-        write_values_bldc(0, 1000, 0, 0);
-
-        vTaskDelay(10000 / portTICK_PERIOD_MS);
-
-        write_values_bldc(0, 0, 1000, 0);
-
-        vTaskDelay(10000 / portTICK_PERIOD_MS);
-
-        write_values_bldc(0, 0, 0, 1000);
-
-        vTaskDelay(10000 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+}
+
+void i2c_init()
+{
+    i2c_port_t port = I2C_NUM_0;
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = 1000000;
+    i2c_param_config(i2c_master_port, &conf);
+    return i2c_driver_install(port, conf.mode, 0, 0, 0);
+}
+
+void i2c_read(uint8_t device_address, unsigned int address, void* buffer, unsigned int bytes_to_read)
+{
+    // get a handle on which you want to queue the i2c communiction commands of write and reads
+    i2c_cmd_handle_t handle i2c_cmd_link_create();
+
+        // send start bit from master
+        i2c_master_start(handle);
+
+            // write address of the device on the bus, last param true signifies we are checking for slave to ack on receive
+            i2c_master_write_byte(cmd, device_address << 1, true);
+
+            // write address from where you want to start reading, last param true signifies we are checking for slave to ack on receive
+            i2c_master_write( ,true);
+
+            // read data from there, but send NACK at the last byte read, instead of ACK
+            i2c_master_read(cmd, buffer, bytes_to_read, I2C_MASTER_LAST_NACK);
+
+        // stop bit from master
+        i2c_master_stop(handle);
+
+        // execute the commands
+        i2c_master_cmd_begin(I2C_NUM_0, handle, );
+
+    // delete the handle once done
+    i2c_cmd_link_delete(handle);
+}
+
+void i2c_write(uint8_t device_address, unsigned int address, void* buffer, unsigned int bytes_to_write)
+{
+    // get a handle on which you want to queue the i2c communiction commands of write and reads 
+    i2c_cmd_handle_t handle i2c_cmd_link_create();
+
+        // send start bit from master
+        i2c_master_start(handle);
+
+            // write device address on the bus
+            i2c_master_write_byte(cmd, device_address << 1, ACK);
+
+            // write the address you want to write to,  last param true signifies we are checking for slave to ack on receive
+            i2c_master_write(, true);
+
+            // write the data,  last param true signifies we are checking for slave to ack on receive
+            i2c_master_write(handle, buffer, bytes_to_write, true);
+
+        // stop bit from master
+        i2c_master_stop(handle);
+
+        // execute the commands
+        i2c_master_cmd_begin(I2C_NUM_0, handle, );
+
+    // delete the handle once done
+    i2c_cmd_link_delete(handle);
+}
+
+void i2c_destroy()
+{
+    i2c_port_t port = I2C_NUM_0;
+    i2c_driver_destroy(port, I2C_MODE_MASTER, 0, 0, 0);
 }
 
 void write_values_bldc(unsigned int left_front, unsigned int right_front, unsigned int left_back, unsigned int right_back)
 {
-
-    #define LEFT_FRONT_MOTOR    33
-    #define RIGHT_FRONT_MOTOR   32
-    #define LEFT_BACK_MOTOR     25
-    #define RIGHT_BACK_MOTOR    26
-
     static unsigned char setup_done = 0;
 
     if(setup_done == 0)
