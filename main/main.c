269e45135540e2b6d96bbcab0fac23751c6863a1
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-10-06 20:50:25 +0530

    corrected i2c logic for read

--- main/main.c
+++ main/main.c
@@ -18,8 +18,7 @@
 
 // i2c addresses, for connected sensors namesly MPU6050 : accl/gyro, HMC5883L : magnetometer and MS5611 : barometer
 #define MPU6050_ADDRESS     0x68
-#define HMC5883L_ADDRESS    0x1e
-#define MS5611_ADDRESS      0x76
+#define MS5611_ADDRESS      0x77
 
 // motor pins
 #define LEFT_FRONT_MOTOR    33
@@ -49,18 +48,20 @@ void app_main(void)
     gpio_pad_select_gpio(BLINK_GPIO);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
-    while(1)
+    uint8_t addr = 0x00;
+
+    while(addr < 0x80)
     {
         gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
         gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
 
-        int16_t accz;
-        i2c_read(MPU6050_ADDRESS, 0x3f, &accz, 2);
+        uint16_t accz;
+        esp_err_t err = i2c_read(addr, 0x3f, &accz, 0);
 
-        printf("accz = %d\n", accz);
-
+        printf("addr = %x, error = %d\n", addr, err);
+        addr++;
     }
 
     i2c_destroy();
@@ -68,14 +69,14 @@ void app_main(void)
 
 void i2c_init()
 {
-    i2c_port_t port = I2C_NUM_0;
+    i2c_port_t port = I2C_NUM_1;
     i2c_config_t conf;
     conf.mode = I2C_MODE_MASTER;
     conf.sda_io_num = I2C_MASTER_SDA_IO;
     conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
     conf.scl_io_num = I2C_MASTER_SCL_IO;
     conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-    conf.master.clk_speed = 1000000;
+    conf.master.clk_speed = 100000;
     i2c_param_config(port, &conf);
     i2c_driver_install(port, conf.mode, 0, 0, 0);
 }
@@ -89,19 +90,25 @@ esp_err_t i2c_read(uint8_t device_address, uint8_t reg_address, void* buffer, un
         i2c_master_start(handle);
 
             // write address of the device on the bus, last param true signifies we are checking for slave to ack on receive
-            i2c_master_write_byte(handle, (device_address << 1) | 1, true);
+            i2c_master_write_byte(handle, (device_address << 1), true);
 
-            // write address from where you want to start reading, last param true signifies we are checking for slave to ack on receive
-            i2c_master_write_byte(handle, reg_address,true);
+            if(bytes_to_read > 0)
+            {
+                // write address from where you want to start reading, last param true signifies we are checking for slave to ack on receive
+                i2c_master_write_byte(handle, reg_address, true);
 
-            // read data from there, but send NACK at the last byte read, instead of ACK
-            i2c_master_read(handle, buffer, bytes_to_read, I2C_MASTER_LAST_NACK);
+                // write address of the device on the bus, last param true signifies we are checking for slave to ack on receive
+                i2c_master_write_byte(handle, (device_address << 1) | 1, true);
+
+                // read data from there, but send NACK at the last byte read, instead of ACK
+                i2c_master_read(handle, buffer, bytes_to_read, I2C_MASTER_LAST_NACK);
+            }
 
         // stop bit from master
         i2c_master_stop(handle);
 
         // execute the commands
-        esp_err_t error = i2c_master_cmd_begin(I2C_NUM_0, handle, 1000 / portTICK_PERIOD_MS);
+        esp_err_t error = i2c_master_cmd_begin(I2C_NUM_1, handle, 1000 / portTICK_PERIOD_MS);
 
     // delete the handle once done
     i2c_cmd_link_delete(handle);
@@ -120,17 +127,20 @@ esp_err_t i2c_write(uint8_t device_address, uint8_t reg_address, void* buffer, u
             // write device address on the bus
             i2c_master_write_byte(handle, device_address << 1, true);
 
-            // write the address you want to write to,  last param true signifies we are checking for slave to ack on receive
-            i2c_master_write_byte(handle, reg_address, true);
+            if(bytes_to_write > 0)
+            {
+                // write the address you want to write to,  last param true signifies we are checking for slave to ack on receive
+                i2c_master_write_byte(handle, reg_address, true);
 
-            // write the data,  last param true signifies we are checking for slave to ack on receive
-            i2c_master_write(handle, buffer, bytes_to_write, true);
+                // write the data,  last param true signifies we are checking for slave to ack on receive
+                i2c_master_write(handle, buffer, bytes_to_write, true);
+            }
 
         // stop bit from master
         i2c_master_stop(handle);
 
         // execute the commands
-        esp_err_t error = i2c_master_cmd_begin(I2C_NUM_0, handle, 1000 / portTICK_PERIOD_MS);
+        esp_err_t error = i2c_master_cmd_begin(I2C_NUM_1, handle, 1000 / portTICK_PERIOD_MS);
 
     // delete the handle once done
     i2c_cmd_link_delete(handle);
@@ -140,7 +150,7 @@ esp_err_t i2c_write(uint8_t device_address, uint8_t reg_address, void* buffer, u
 
 void i2c_destroy()
 {
-    i2c_port_t port = I2C_NUM_0;
+    i2c_port_t port = I2C_NUM_1;
     i2c_driver_delete(port);
 }
 
@@ -172,8 +182,7 @@ void write_values_bldc(unsigned int left_front, unsigned int right_front, unsign
         setup_done = 1;
     }
 
-
-
+    // set motor speed values
     mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, (left_front  > 1000 ? 1000 : left_front ) + 1000);
     mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, (right_front > 1000 ? 1000 : right_front) + 1000);
     mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_1, MCPWM_OPR_A, (left_back   > 1000 ? 1000 : left_back  ) + 1000);
