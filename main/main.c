6068e5441ab52758f9a59e6efcb24a2eac97259d
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-10-08 02:16:02 +0530

    proper scaling of the sensor data was done appropriately

--- main/main.c
+++ main/main.c
@@ -71,8 +71,8 @@ void channels_destroy();
 
 // mpu data
 void imu_init();
-esp_err_t get_raw_IMUdata(IMUdata* data);
-
+esp_err_t get_raw_IMUdata(IMUdata* data, uint8_t mpu_data, uint8_t hmc_data);
+void scale_IMUdata(IMUdatascaled* result, IMUdata* data, uint8_t mpu_data, uint8_t hmc_data);
 
 // basic i2c functionality, could be similar for most sensors, being user for mpu6050 and will be used for ms5611
 void i2c_init();
@@ -102,12 +102,14 @@ void app_main(void)
         vTaskDelay(100 / portTICK_PERIOD_MS);
 
         IMUdata data;
-        get_raw_IMUdata(&data);
+        IMUdatascaled datas;
+        get_raw_IMUdata(&data, 1, 1);
+        scale_IMUdata(&datas, &data, 1, 1);
 
-        printf("accl : \t%d \t%d \t%d\n", data.acclx, data.accly, data.acclz);
-        printf("gyro : \t%d \t%d \t%d\n", data.gyrox, data.gyroy, data.gyroz);
-        printf("magn : \t%d \t%d \t%d\n", data.magnx, data.magny, data.magnz);
-        printf("temp : \t%d\n\n", data.temp);
+        printf("accl : \t%lf \t%lf \t%lf\n", datas.acclx, datas.accly, datas.acclz);
+        printf("gyro : \t%lf \t%lf \t%lf\n", datas.gyrox, datas.gyroy, datas.gyroz);
+        printf("magn : \t%lf \t%lf \t%lf\n", datas.magnx, datas.magny, datas.magnz);
+        printf("temp : \t%lf\n\n", datas.temp);
     }
     while(1);
 
@@ -193,6 +195,8 @@ void channels_destroy()
     gpio_uninstall_isr_service();
 }
 
+static IMUdatascaled offsets = {.acclx = 0, .accly = 0, .acclz = 0, .gyrox = 0, .gyroy = 0, .gyroz = 0, .temp = 0, .magnx = 0, .magny = 0, .magnz = 0};
+
 void imu_init()
 {
     uint8_t data;
@@ -247,39 +251,87 @@ void imu_init()
     data = data & (~(1 << 1));
     i2c_write(MPU6050_ADDRESS, 0x37, &data, 1);
 */
+
+    for(uint16_t i = 0; i < 500; i++)
+    {
+        IMUdata data;
+        get_raw_IMUdata(&data, 1, 1);
+        offsets.acclx += (((double)(data.acclx))/500);
+        offsets.accly += (((double)(data.accly))/500);
+        offsets.acclz += ((((double)(data.acclz))-16384)/500);
+        offsets.gyrox += (((double)(data.gyrox))/500);
+        offsets.gyroy += (((double)(data.gyroy))/500);
+        offsets.gyroz += (((double)(data.gyroz))/500);
+        offsets.magnx += (((double)(data.magnx))/500);
+        offsets.magny += (((double)(data.magny))/500);
+        offsets.magnz += (((double)(data.magnz))/500);
+        vTaskDelay(14 / portTICK_PERIOD_MS);
+    }
 }
 
-esp_err_t get_raw_IMUdata(IMUdata* data)
+esp_err_t get_raw_IMUdata(IMUdata* data, uint8_t mpu_data, uint8_t hmc_data)
 {
-    esp_err_t err;
+    esp_err_t err = ESP_OK;
 
-    err = i2c_read(MPU6050_ADDRESS, 0x3b, data, 14);
-    if(err != ESP_OK)
+    if(mpu_data)
     {
-        return err;
+        err = i2c_read(MPU6050_ADDRESS, 0x3b, data, 14);
+        if(err != ESP_OK)
+        {
+            return err;
+        }
+
+        // we have to change the data from network ordr to host order integers
+        data->acclx = (data->acclx << 8) | ((data->acclx >> 8) & 0x00ff);
+        data->accly = (data->accly << 8) | ((data->accly >> 8) & 0x00ff);
+        data->acclz = (data->acclz << 8) | ((data->acclz >> 8) & 0x00ff);
+        data->temp = (data->temp << 8) | ((data->temp >> 8) & 0x00ff);
+        data->gyrox = (data->gyrox << 8) | ((data->gyrox >> 8) & 0x00ff);
+        data->gyroy = (data->gyroy << 8) | ((data->gyroy >> 8) & 0x00ff);
+        data->gyroz = (data->gyroz << 8) | ((data->gyroz >> 8) & 0x00ff);
     }
 
-    // we have to change the data from network ordr to host order integers
-    data->acclx = (data->acclx << 8) | ((data->acclx >> 8) & 0x00ff);
-    data->accly = (data->accly << 8) | ((data->accly >> 8) & 0x00ff);
-    data->acclz = (data->acclz << 8) | ((data->acclz >> 8) & 0x00ff);
-    data->temp = (data->temp << 8) | ((data->temp >> 8) & 0x00ff);
-    data->gyrox = (data->gyrox << 8) | ((data->gyrox >> 8) & 0x00ff);
-    data->gyroy = (data->gyroy << 8) | ((data->gyroy >> 8) & 0x00ff);
-    data->gyroz = (data->gyroz << 8) | ((data->gyroz >> 8) & 0x00ff);
-
-    err = i2c_read(HMC5883_ADDRESS, 0x03, ((void*)data) + 14, 6);
-    if(err != ESP_OK)
+    if(hmc_data)
     {
-        return err;
-    }
+        err = i2c_read(HMC5883_ADDRESS, 0x03, ((void*)data) + 14, 6);
+        if(err != ESP_OK)
+        {
+            return err;
+        }
 
-    // we have to change the data from network ordr to host order integers
-    data->magnx = (data->magnx << 8) | ((data->magnx >> 8) & 0x00ff);
-    data->magny = (data->magny << 8) | ((data->magny >> 8) & 0x00ff);
-    data->magnz = (data->magnz << 8) | ((data->magnz >> 8) & 0x00ff);
+        // we have to change the data from network ordr to host order integers
+        data->magnx = (data->magnx << 8) | ((data->magnx >> 8) & 0x00ff);
+        data->magny = (data->magny << 8) | ((data->magny >> 8) & 0x00ff);
+        data->magnz = (data->magnz << 8) | ((data->magnz >> 8) & 0x00ff);
+    }
 
     return err;
+}
+
+void scale_IMUdata(IMUdatascaled* result, IMUdata* data, uint8_t mpu_data, uint8_t hmc_data)
+{
+    if(mpu_data)
+    {
+        // in m/s2, meter per second square
+        result->acclx = ((((double)(data->acclx)) - offsets.acclx) * 19.6) / 32768.0;
+        result->accly = ((((double)(data->accly)) - offsets.accly) * 19.6) / 32768.0;
+        result->acclz = ((((double)(data->acclz)) - offsets.acclz) * 19.6) / 32768.0;
+
+        result->temp  = ((double)(data->temp));
+
+        // in dps, degrees per second
+        result->gyrox = ((((double)(data->gyrox)) - offsets.gyrox) * 250.0) / 32768.0;
+        result->gyroy = ((((double)(data->gyroy)) - offsets.gyroy) * 250.0) / 32768.0;
+        result->gyroz = ((((double)(data->gyroz)) - offsets.gyroz) * 250.0) / 32768.0;
+    }
+
+    // in mG, milli Gauss
+    if(hmc_data)
+    {
+        result->magnx = ((((double)(data->magnx)) - offsets.magnx) * 0.92);
+        result->magny = ((((double)(data->magny)) - offsets.magny) * 0.92);
+        result->magnz = ((((double)(data->magnz)) - offsets.magnz) * 0.92);
+    }
 }
 
 void i2c_init()
