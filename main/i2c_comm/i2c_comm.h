767f70bbfe57cb317c10bf5f719b0fea6297382f
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-10-12 20:53:38 +0530

    completely divided the project into multiple folders respective to their task

--- main/i2c_comm/i2c_comm.h
+++ main/i2c_comm/i2c_comm.h
@@ -4,6 +4,9 @@
 // for i2c for sensors
 #include "driver/i2c.h"
 
+#define I2C_MASTER_SCL_IO   22
+#define I2C_MASTER_SDA_IO   21
+
 // basic i2c functionality, could be similar for most sensors, being user for mpu6050 and will be used for ms5611
 void i2c_init();
 esp_err_t i2c_read(uint8_t device_address, uint8_t reg_address, void* buffer, unsigned int bytes_to_read);
